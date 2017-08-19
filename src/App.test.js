4bcf89a43d6b75102b41239bfceeca5245eabd2b
Paul Cuichan <ing.paul.cuixan@gmail.com>
2017-08-19 18:25:02 -0500

    Se configuro eslint de forma optima

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@
 import React from 'react'// eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom'
 import App from './App'// eslint-disable-line no-unused-vars
-it('renders without crashing', () => { // eslint-disable-line no-undef
+it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
 })
